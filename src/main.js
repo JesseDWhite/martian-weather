45eb9a774c2ced44a369b60f316efd97238ed713
Jesse White <jesse.white6@gmail.com>
2021-04-21 14:22:01 -0700

    added everything.
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,3 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import WeatherService from './js/weather-service.js'
 
+function getElements(response) {
+    if (response[845]) {
+        $("#date-today").text(`This data was pulled from ${Date(response[845].First_UTC)}`)
+        $("#season").text(`The current season is: ${response[845].Northern_season}`)
+        $("#pressure-elements").html(`<li>${response[845].PRE.av}</li> <li>${response[845].PRE.ct}</li> <li>${response[845].PRE.mn}</li> <li>${response[845].PRE.mx}</li>`)
+    } else {
+        $("#show-errors").text(`${response}`)
+    }
+}
+
+$(document).ready(function () {
+    $("#main-page").submit(function (event) {
+        event.preventDefault();
+
+        WeatherService.getWeather()
+            .then(function (response) {
+                getElements(response)
+            });
+    });
+});
